810d9d79ca9f5c7c8a352184a8918deed54c1839
Haojia-biostat <haojial412@gmail.com>
2023-03-21 14:31:18 -0600

    Update smote cpp function

--- src/smote.cpp
+++ src/smote.cpp
@@ -1,59 +1,80 @@
 #include <Rcpp.h>
-#include <algorithm>
-
 using namespace Rcpp;
 
 // [[Rcpp::export]]
-NumericMatrix smoteCpp(const NumericMatrix& X, const IntegerVector& y, const int k, const int m) {
-  
+
+//@name X feature matrix
+//@name k number of nearest neighbors to be considered, default value is 5
+//@name N number of new synthetic examples to be generated, default value is 9
+
+// matrix of euclidean distance
+NumericMatrix SMOTE(
+    const NumericMatrix& X,
+    const int& k = 5,
+    const int& N = 9
+) {
   int n = X.nrow();
   int p = X.ncol();
-  
-  NumericMatrix X_resampled(n*m, p);
-  IntegerVector y_resampled(n*m);
-  
-  int index = 0;
-  for (int i = 0; i < n; i++) {
-    
-    if (y(i) == 1) {
-      X_resampled(index, _) = X(i, _);
-      y_resampled[index] = y(i);
-      index++;
-    } else {
-      NumericVector x(p);
-      for (int j = 0; j < p; j++) {
-        x[j] = X(i, j);
+
+  if(k > n-1)
+    stop("k (number of nearest neighbors) is larger than the number of observations - 1. Please select a smaller k value.");
+
+  // initialize matrix of synthetic examples
+  NumericMatrix new_synthetic_examples(n*N, p);
+  // initialize row index of output matrix
+  int row_ind = 0;
+  // initialize vector of random number between 0 and 1
+  NumericVector rand_vec = runif(n*N);
+
+  for(int i = 0; i < n; ++i) {
+
+    NumericVector x = X(i, _);
+
+    // matrix of difference
+    NumericMatrix difference(n, p);
+    // vector of Euclidean distance
+    NumericVector euclidean_vec(n);
+
+    for(int j = 0; j < n; ++j) {
+
+      // initialize cumulative sum of squared difference
+      double cum_sqdiff = 0;
+
+      for(int k = 0; k < p; ++k) {
+        difference(j,k) = X(j,k) - x[k];
+        cum_sqdiff += pow(X(j,k) - x[k], 2);
       }
-      NumericMatrix nn(n, 1);
-      for (int j = 0; j < n; j++) {
-        double dist = 0;
-        for (int l = 0; l < p; l++) {
-          dist += pow(X(j, l) - x[l], 2);
-        }
-        nn(j, 0) = sqrt(dist);
-      }
-      IntegerVector k_nn_indices = nn(_, 0).std::sort_indexes()[Range(0, k)];
-      for (int j = 0; j < m; j++) {
-        int index_nn = k_nn_indices[rand() % k];
-        NumericVector x_nn(p);
-        for (int l = 0; l < p; l++) {
-          x_nn[l] = X(index_nn, l);
-        }
-        NumericVector x_new(p);
-        for (int l = 0; l < p; l++) {
-          x_new[l] = x[l] + (x_nn[l] - x[l]) * (double)(rand() % 101) / 100.0;
-        }
-        X_resampled(index, _) = x_new;
-        y_resampled[index] = y(i);
-        index++;
-      }
+
+      euclidean_vec[k] = sqrt(cum_sqdiff);
     }
+
+    // sort indices by Euclidean distance
+    IntegerVector sorted_ind = seq(0, n-1);
+    std::sort(sorted_ind.begin(), sorted_ind.end(), [&euclidean_vec](int a, int b) { return euclidean_vec[a] < euclidean_vec[b]; });
+
+    // select top k indices
+    IntegerVector k_seq = seq(1, k);
+    IntegerVector knn_ind = sorted_ind[k_seq];
+
+    // sample N from k
+    IntegerVector synN_ind;
+    if(N <= k)
+      synN_ind = sample(knn_ind, N, false);
+    else
+      synN_ind = sample(knn_ind, N, true);
+
+    for(int m = 0; m < N; ++m) {
+
+      for(int k = 0; k < p; ++k) {
+        // generate synthetic example by interpolation
+        new_synthetic_examples(row_ind, k) = x[k] + rand_vec[row_ind] * difference(synN_ind[m], k);
+      }
+
+      row_ind += 1;
+    }
+
   }
-  
-  NumericMatrix resampled_data(index, p + 1);
-  for (int i = 0; i < index; i++) {
-    resampled_data(i, _) = c(X_resampled(i, _), y_resampled[i]);
-  }
-  
-  return resampled_data;
+
+  return new_synthetic_examples;
+
 }
