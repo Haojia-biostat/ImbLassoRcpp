4da51d47ddc0cddb3380ec165c0b6c8fc46245f5
Haojia-biostat <haojial412@gmail.com>
2023-03-29 22:47:08 -0600

    Update the document of lambda_gen.cpp, including description, value and references.

--- src/lambda_gen.cpp
+++ src/lambda_gen.cpp
@@ -2,9 +2,13 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 
-//'calculate the entry level of lambda
+//'Generate the lambda sequence for LASSO regression
 //'
-//'Calculate the smallest value for which all the beta equal to 0.
+//'This function computes the solutions for a decreasing sequence of values for \eqn{\lambda},
+//'starting at the smallest value \eqn{\lambda_{max}} for which the entire vector \eqn{\hat{\beta} = 0}.
+//'Then the minimum value \eqn{\lambda_{min}} is determined as \eqn{\epsilon\lambda_{max}},
+//'where \eqn{\epsilon = 0.0001} if \code{nobs > nvars}, otherwise \eqn{\epsilon = 0.01}.
+//'A sequence of \eqn{K} values of \eqn{\lambda} decreasing from \eqn{\lambda_{max}} to \eqn{\lambda_{min}} on the log scale.
 //'
 //'@name lambda_gen
 //'
@@ -12,8 +16,16 @@ using namespace Rcpp;
 //'@param y binary response variable, positive outcome = 1 and negative outcome = 0.
 //'@param K number of lambdas, by default is 100.
 //'
-//'@return A number
+//'@return A list including:
+//'\item {Entry value} {\eqn{\lambda_{max}}}
+//'\item {Minimum ratio} {\eqn{\epsilon}}
+//'\item {Lambdas} {\eqn{K} values of \eqn{\lambda} decreasing from \eqn{\lambda_{max}} to \eqn{\lambda_{min}} on the log scale}
 //'
+//'@references
+//'Friedman, J., Hastie, T. and Tibshirani, R. (2008)
+//'\emph {Regularization Paths for Generalized Linear Models via Coordinate Descent (2010),
+//'Journal of Statistical Software, Vol. 33(1), 1-22},
+//'\doi{}10.18637/jss.v033.i01.}
 //'
 //'@export
 
